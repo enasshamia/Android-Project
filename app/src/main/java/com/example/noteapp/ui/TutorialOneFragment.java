9255208ad305de297dc3b6d447cbc67152d4b0d4
Enas Shamia <enasshamia1@gmail.com>
2019-12-21 00:43:49 +0300

    Notebook Tutorial 1

---
+++ app/src/main/java/com/example/noteapp/ui/TutorialOneFragment.java
@@ -0,0 +1,32 @@
+package com.example.noteapp.ui;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.noteapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TutorialOneFragment extends Fragment {
+
+
+    public TutorialOneFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tutorial_one, container, false);
+    }
+
+}
